6530175cdbe8305893de7f2c5b375688eeb849c4
Oleksiy <58302277+porkbelly-lab@users.noreply.github.com>
2020-04-30 08:57:43 +0300

    Update task2.kts

--- CodeWarsLabs/Task2/task2.kts
+++ CodeWarsLabs/Task2/task2.kts
@@ -1,1 +1,28 @@
+fun getCount(str : String) : Int {
+    var Count = 0;
+
+    for (x in str) {
+        if (x == 'a') {
+            Count++;
+        }
+        else if (x == 'e') {
+            Count++;
+        }
+        else if (x == 'i') {
+            Count++;
+        }
+        else if (x == 'o') {
+            Count++;
+        }
+        else if (x == 'u') {
+            Count++;
+        }
+
+    }
+    println("$Count")
+    return Count;
+}
+fun main(args: Array<String>) {
+getCount("mami ell on under")
+}
 

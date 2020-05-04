aa6aade67b7ab4b5b745c049cd8d13e88516deb8
Oleksiy <58302277+porkbelly-lab@users.noreply.github.com>
2020-05-04 14:37:24 +0300

    Update task3.kts

--- CodeWarsLabs/Task3/task3.kts
+++ CodeWarsLabs/Task3/task3.kts
@@ -1,1 +1,22 @@
+package maxrot
 
+fun maxRot(n:Long):Long {
+    var i = (n).toString()
+    var ZeroChar = ""
+    var mx = n
+    if (i.length == 1) return n
+    while (true)
+    {
+      // переміщення вліво
+      var r = i.substring(1) + i.get(0)
+      i = r
+      // Заморозимо першу змінну
+      ZeroChar += i.get(0)
+      // фіналочка
+      i = i.substring(1)
+      val nb = java.lang.Long.parseLong(ZeroChar + i)
+      if (nb > mx) mx = nb
+      if (i.length == 1) break
+    }
+    return mx
+}

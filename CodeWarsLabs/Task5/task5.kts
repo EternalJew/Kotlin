f78356192e5404e53bfd6c33ba0144edd2c9692f
Oleksiy <58302277+porkbelly-lab@users.noreply.github.com>
2020-05-09 16:16:51 +0300

    Update task5.kts

--- CodeWarsLabs/Task5/task5.kts
+++ CodeWarsLabs/Task5/task5.kts
@@ -1,1 +1,17 @@
+package growth
 
+fun nbYear(p0:Int, percent:Double, aug:Int, p:Int): Int {
+    var a = p0
+    var n = 0;
+    while (a < p) {
+        n++
+        a = (a + (a*percent/100) + aug).toInt();
+    }
+    if (a >= p) {
+        return n;
+    }
+    return n;
+}
+fun main(args: Array<String>) {
+nbYear(1000,2.0,50,1200)
+}

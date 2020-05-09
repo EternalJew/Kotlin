93a3a31d375b801f24b7858f608443fc7eb7bd48
Oleksiy <58302277+porkbelly-lab@users.noreply.github.com>
2020-05-09 14:45:57 +0300

    Update task4.kts

--- CodeWarsLabs/Task4/task4.kts
+++ CodeWarsLabs/Task4/task4.kts
@@ -1,1 +1,21 @@
+fun highAndLow() {
+    val numbers: IntArray = intArrayOf(-7, 9, 3, 2, 6)
+    var largestElement = numbers[0]
 
+    for (number in numbers){
+        if(largestElement < number)
+            largestElement = number
+
+    }
+    println("Найдовше число в масиві: $largestElement")
+    var shortestElement = numbers[0]
+    for(number in numbers){
+        if(shortestElement > number)
+            shortestElement = number
+
+    }
+    println("Найкоротше число в масиві: $shortestElement")
+}
+fun main(args: Array<String>) {
+highAndLow()
+}

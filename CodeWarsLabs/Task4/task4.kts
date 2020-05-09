c2deb7345bcc15364fb8ab8a2730a7c9dc9b41ba
Oleksiy <58302277+porkbelly-lab@users.noreply.github.com>
2020-05-09 15:36:29 +0300

    Update task4.kts

--- CodeWarsLabs/Task4/task4.kts
+++ CodeWarsLabs/Task4/task4.kts
@@ -1,20 +1,27 @@
 fun highAndLow() {
-    val numbers: IntArray = intArrayOf(-7, 9, 3, 2, 6)
-    var largestElement = numbers[0]
+    val str = "1 2 3 4 5 6 7"
+    val strArr = str.split(" ").toTypedArray()
+    val numArr = IntArray(strArr.size)
+    for (i in strArr.indices) {
+        numArr[i] = strArr[i].toInt()
+    }
+    var largestElement = numArr[0]
 
-    for (number in numbers){
+    for (number in numArr){
         if(largestElement < number)
             largestElement = number
 
     }
     println("Найдовше число в масиві: $largestElement")
-    var shortestElement = numbers[0]
-    for(number in numbers){
+    var shortestElement = numArr[0]
+    for(number in numArr){
         if(shortestElement > number)
             shortestElement = number
 
     }
     println("Найкоротше число в масиві: $shortestElement")
+
+
 }
 fun main(args: Array<String>) {
 highAndLow()

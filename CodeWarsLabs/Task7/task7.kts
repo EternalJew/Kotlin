4e6db14d0f7261b6041bfcc902a1ea05821eefa7
Oleksiy <58302277+porkbelly-lab@users.noreply.github.com>
2020-05-11 14:58:35 +0300

    Update task7.kts

--- CodeWarsLabs/Task7/task7.kts
+++ CodeWarsLabs/Task7/task7.kts
@@ -1,1 +1,16 @@
+ fun findShort(s: String): Int {
+        val arr = s.split(" ").toTypedArray()
+        var min = arr[0].length
+        for (i in arr) {
+            if (min > i.length) {
+                min = i.length
+            }
+        }
+        println("$min")
+        return min
 
+    }
+
+    fun main(args: Array<String>) {
+        findShort("lets talk about Java the best language")
+}

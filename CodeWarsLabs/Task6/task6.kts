8b71eda66bb2bdb3df2cd952437888004e7c0906
Oleksiy <58302277+porkbelly-lab@users.noreply.github.com>
2020-05-11 14:30:36 +0300

    Update task6.kts

--- CodeWarsLabs/Task6/task6.kts
+++ CodeWarsLabs/Task6/task6.kts
@@ -1,1 +1,19 @@
-
+fun inviteMoreWomen(){
+    var arr = arrayOf(1,-1,1)
+val a: Boolean= true
+    val b: Boolean = false
+    var count = 0
+    for(el in arr){
+        if(el == 1) count++
+        else if(el == -1) count--
+    }
+if (count == 0){
+    println("$b")
+}
+    else{
+    println("$a")
+    }
+}
+fun main(args: Array<String>) {
+    inviteMoreWomen()
+}

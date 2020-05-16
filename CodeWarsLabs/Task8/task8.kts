760b38f265a7ca3c00b8ad9744f5a77f2a3b3802
Oleksiy <58302277+porkbelly-lab@users.noreply.github.com>
2020-05-16 16:04:44 +0300

    Update task8.kts

--- CodeWarsLabs/Task8/task8.kts
+++ CodeWarsLabs/Task8/task8.kts
@@ -3,3 +3,25 @@
 //      7     9    11
 //   13    15    17    19
 //21    23    25    27    29
+fun rowSumOddNumbers(n:Int) {
+    var arr = arrayOf(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29)
+        var firstIndex = 0
+        for (i in 1..n) {
+            firstIndex += i
+        }
+        var total = 0
+        var i = firstIndex
+        var h = firstIndex + n
+        for (i in i..h) {
+            total += arr[i]
+        }
+    println("$total")
+}
+fun main(args: Array<String>) {
+    rowSumOddNumbers(0)
+    rowSumOddNumbers(1)
+    rowSumOddNumbers(2)
+    rowSumOddNumbers(3)
+    rowSumOddNumbers(4)
+  
+}
